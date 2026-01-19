ccb59fbe7f6500ad2e797a5226c3c797ed7f1bad
lott-ai <7423098+lott-ai@users.noreply.github.com>
2026-01-19 06:06:40 +0000

    chore: update formula for v0.2.0

--- Formula/brewlock.rb
+++ Formula/brewlock.rb
@@ -4,14 +4,14 @@
 class Brewlock < Formula
   desc "The missing lock file for Homebrew"
   homepage "https://github.com/ShipWorthyAI/brewlock"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   depends_on arch: :arm64
   depends_on :macos
 
-  url "https://github.com/ShipWorthyAI/brewlock/releases/download/v0.1.0/brewlock-darwin-arm64.tar.gz"
-  sha256 "a813e364365faf6963f54b2eb76c8e71c31aaab59f3b8196485d455d1ac99659"
+  url "https://github.com/ShipWorthyAI/brewlock/releases/download/v0.2.0/brewlock-darwin-arm64.tar.gz"
+  sha256 "ecd86a91d9696aec3871fc676f894b3e506a3e120703e5729c6bee74abb8092c"
 
   def install
     bin.install "brewlock"
